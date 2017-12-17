84c6b29a51d8290ac888f76e0f3317023643098f
psyomn <lethaljellybean@gmail.com>
2017-12-17 12:51:51 -0500

    Update unrolled list tests

--- test/unrolled_list_example.c
+++ test/unrolled_list_example.c
@@ -1,55 +1,49 @@
 #include <bds/unrolled_list.h>
+#include <bds/test.h>
 
 #include <stdio.h>
 #include <assert.h>
 
-#define LABEL_FUNC do { fprintf(stdout, "#### %s:%s\n", __FILE__, __FUNCTION__); } while (0)
-
 void add_none()
 {
-  LABEL_FUNC;
-
-  struct bds_unrolled_list* ulist = bds_unrolled_list_new(2 << 8);
-  const size_t first_num_nodes = bds_unrolled_list_num_nodes(ulist);
-  fprintf(stdout, "num nodes before: %zu\n", first_num_nodes);
-  assert(first_num_nodes == 0);
-  bds_unrolled_list_free(ulist);
+  BDS_TEST("add none", {
+      struct bds_unrolled_list* ulist = bds_unrolled_list_new(2 << 8);
+      const size_t first_num_nodes = bds_unrolled_list_num_nodes(ulist);
+      assert(first_num_nodes == 0);
+      bds_unrolled_list_free(ulist);
+    });
 }
 
 void add_one()
 {
-  LABEL_FUNC;
+  BDS_TEST("add one", {
+      struct bds_unrolled_list* ulist = bds_unrolled_list_new(2 << 8);
+      const size_t first_num_nodes = bds_unrolled_list_num_nodes(ulist);
+      assert(first_num_nodes == 0);
 
-  struct bds_unrolled_list* ulist = bds_unrolled_list_new(2 << 8);
-  const size_t first_num_nodes = bds_unrolled_list_num_nodes(ulist);
-  fprintf(stdout, "num nodes before: %zu\n", first_num_nodes);
-  assert(first_num_nodes == 0);
+      bds_unrolled_list_push_back(ulist, 1);
+      const size_t add_two = bds_unrolled_list_num_nodes(ulist);
+      assert(add_two == 1);
 
-  bds_unrolled_list_push_back(ulist, 1);
-  const size_t add_two = bds_unrolled_list_num_nodes(ulist);
-  fprintf(stdout, "num nodes after: %zu\n", add_two);
-  assert(add_two == 1);
-
-  bds_unrolled_list_free(ulist);
+      bds_unrolled_list_free(ulist);
+    });
 }
 
 void mash_new_nodes()
 {
-  LABEL_FUNC;
+  BDS_TEST("mash new nodes", {
+      struct bds_unrolled_list* ulist = bds_unrolled_list_new(2 << 7);
+      const size_t first_num_nodes = bds_unrolled_list_num_nodes(ulist);
+      assert(first_num_nodes == 0);
 
-  struct bds_unrolled_list* ulist = bds_unrolled_list_new(2 << 7);
-  const size_t first_num_nodes = bds_unrolled_list_num_nodes(ulist);
-  fprintf(stdout, "num nodes before: %zu\n", first_num_nodes);
-  assert(first_num_nodes == 0);
+      for (size_t x = 0; x < (2 << 7); ++x)
+        bds_unrolled_list_push_back(ulist, x);
 
-  for (size_t x = 0; x < (2 << 7); ++x)
-    bds_unrolled_list_push_back(ulist, x);
+      const size_t num_nodes = bds_unrolled_list_num_nodes(ulist);
+      assert(num_nodes == 2);
 
-  const size_t num_nodes = bds_unrolled_list_num_nodes(ulist);
-  fprintf(stdout, "num nodes after bruteforcing: %zu\n", num_nodes);
-  assert(num_nodes == 2);
-
-  bds_unrolled_list_free(ulist);
+      bds_unrolled_list_free(ulist);
+    });
 }
 
 int main(void)
@@ -57,6 +51,5 @@ int main(void)
   add_none();
   add_one();
   mash_new_nodes();
-
   return 0;
 }
